43dfd61792d5804b9e016f1bebab339391847e8e
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-09-16 01:12:14 +0700

    feat: add currency validation rule

--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/suryadharma5/gobank/db/sqlc"
 )
 
@@ -19,9 +21,15 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
+
+	router.POST("/transfer", server.createTransfer)
 
 	server.router = router
 	return server
---
+++ util/currency.go
@@ -0,0 +1,16 @@
+package util
+
+const (
+	USD = "USD"
+	EUR = "EUR"
+	IDR = "IDR"
+)
+
+func IsSupportedCurrency(currency string) bool {
+	switch currency {
+	case USD, EUR, IDR:
+		return true
+	}
+
+	return false
+}
