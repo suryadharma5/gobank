f50f332168d48c5ccc7a7e5f7879a4be98341270
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-09-16 01:12:39 +0700

    refactor: change validator to currency validator

---
+++ api/transfer.go
@@ -0,0 +1,68 @@
+package api
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/suryadharma5/gobank/db/sqlc"
+)
+
+type createTransferRequest struct {
+	FromAccountID int64  `json:"from_account_id" binding:"required,min=1"`
+	ToAccountID   int64  `json:"to_account_id" binding:"required,min=1"`
+	Amount        int64  `json:"amount" binding:"required,gt=0"`
+	Currency      string `json:"currency" binding:"required,currency"`
+}
+
+func (server *Server) createTransfer(ctx *gin.Context) {
+	var req createTransferRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	if !server.isValidAccount(ctx, req.FromAccountID, req.Currency) {
+		return
+	}
+
+	if !server.isValidAccount(ctx, req.ToAccountID, req.Currency) {
+		return
+	}
+
+	arg := db.TransferTxParams{
+		FromAccountID: req.FromAccountID,
+		ToAccountID:   req.ToAccountID,
+		Amount:        req.Amount,
+	}
+
+	result, err := server.store.TransferTx(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, result)
+}
+
+func (server *Server) isValidAccount(ctx *gin.Context, accountId int64, currency string) bool {
+	account, err := server.store.GetAccount(ctx, accountId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return false
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return false
+	}
+
+	if account.Currency != currency {
+		err := fmt.Errorf("account [%d] currency missmatch => expected: %v, received: %v", accountId, account.Currency, currency)
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return false
+	}
+
+	return true
+}
